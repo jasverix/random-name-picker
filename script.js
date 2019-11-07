8858b00f83b75d30c8441ce0d6fe8a4e48ecb5c6
Jan-Sverre Riksfjord <jasverix@gmail.com>
2019-11-07 20:10:52 +0100

    move HTML-elements to an object initialized after DOM is ready

--- script.js
+++ script.js
@@ -2,9 +2,44 @@
   let timeout = 10
   const maxTimeout = 1000
 
-  let shuffleNames = () => {
+  const elements = {
+    nameTextArea: null,
+    startButton: null,
+    winnerName: null,
+    winnerTitle: null,
   }
-  let hideNames = () => {
+
+  const shuffleNames = () => {
+    const names = elements.nameTextArea.value.split('\n').filter(n => n !== '')
+
+    if (names.length > 0) {
+      elements.startButton.style.visibility = 'hidden'
+
+      const index = Math.floor(Math.random() * names.length)
+      const name = elements.winnerName.innerHTML
+
+      if (names[index] === '' || names[index] === null || names[index] === void 0) {
+        shuffleNames()
+      } else {
+        if (timeout > maxTimeout && name !== '') {
+          // display winner
+          elements.winnerTitle.style.visibility = 'visible'
+          elements.nameTextArea.value = elements.nameTextArea.value.replace(name, '') // remove all entries of 'name' from text area
+
+          // display new start button after 5 secs
+          window.setTimeout(() => {
+            elements.startButton.style.visibility = 'visible'
+          }, 5000)
+        } else {
+          elements.winnerTitle.style.visibility = 'hidden'
+          elements.winnerName.innerHTML = names[index]
+
+          // for each run, increase timeout for next name pick, until timeout exceeds maxTimeout, then winner is displayed
+          timeout = Math.round(timeout * 1000 / 900)
+          window.setTimeout(shuffleNames, timeout)
+        }
+      }
+    }
   }
 
   function initialize () {
@@ -13,45 +48,10 @@
     const winnerNameElement = document.getElementById('divWinnerName')
     const winnerTitleElement = document.getElementById('divWinnerTitle')
 
-    shuffleNames = function shuffleNames () {
-      const names = nameTextArea.value.split('\n').filter(n => n !== '')
-
-      if (names.length > 0) {
-        startButton.style.visibility = 'hidden'
-
-        const index = Math.floor(Math.random() * names.length)
-        const name = winnerNameElement.innerHTML
-
-        if (names[index] === '' || names[index] === null || names[index] === void 0) {
-          shuffleNames()
-        } else {
-          if (timeout > maxTimeout && name !== '') {
-            // display winner
-            winnerTitleElement.style.visibility = 'visible'
-            nameTextArea.value = nameTextArea.value.replace(name, '')
-
-            // display new start button after 5 secs
-            window.setTimeout(() => {
-              startButton.style.visibility = 'visible'
-            }, 5000)
-          } else {
-            winnerTitleElement.style.visibility = 'hidden'
-            winnerNameElement.innerHTML = names[index]
-
-            // for each run, increase timeout for next name pick, until timeout exceeds maxTimeout, then winner is displayed
-            timeout = Math.round(timeout * 1000 / 900)
-            window.setTimeout(shuffleNames, timeout)
-          }
-        }
-      }
-    }
-
-    hideNames = function hideNames () {
-      document.getElementById('divNames').style.display = 'none'
-      document.getElementById('divTrekning').style.display = 'block'
-      winnerNameElement.innerHTML = ''
-      winnerTitleElement.style.visibility = 'hidden'
-    }
+    elements.nameTextArea = nameTextArea
+    elements.startButton = startButton
+    elements.winnerName = winnerNameElement
+    elements.winnerTitle = winnerTitleElement
   }
 
   window.addEventListener('DOMContentLoaded', () => initialize())
@@ -66,7 +66,10 @@
      * Hide name textarea and display the "Start" button
      */
     hideNames () {
-      hideNames()
+      document.getElementById('divNames').style.display = 'none'
+      document.getElementById('divTrekning').style.display = 'block'
+      elements.winnerName.innerHTML = ''
+      elements.winnerTitle.style.visibility = 'hidden'
     },
   }
 }())
